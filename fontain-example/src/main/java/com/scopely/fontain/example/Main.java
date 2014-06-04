a68259f51a4ab62896ded14c5d3d6ff5225c7476
Peter Attardo <Peter@Scopely.com>
2014-06-03 18:55:05 -0700

    Add default font family in example app as string resource

--- fontain-example/src/main/java/com/scopely/fontain/example/Main.java
+++ fontain-example/src/main/java/com/scopely/fontain/example/Main.java
@@ -20,7 +20,7 @@ public class Main extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fontain.init(this, "PTSans");
+        Fontain.init(this, R.string.default_font_family);
         Fontain.applyFontToViewHierarchy(getActionBarView(), Fontain.getFontFamily("MedievalSharp"), Weight.NORMAL.value, Width.NORMAL.value, Slope.NORMAL.value);
         setContentView(R.layout.activity_main);
     }
