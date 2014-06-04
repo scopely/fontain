96830ff9f3daa86c014a41b5420ae88e567a97e6
Peter Attardo <Peter@Scopely.com>
2014-06-04 14:14:37 -0700

    Update example project

--- fontain-example/src/main/java/com/scopely/fontain/example/Main.java
+++ fontain-example/src/main/java/com/scopely/fontain/example/Main.java
@@ -2,9 +2,11 @@ package com.scopely.fontain.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 
 import com.scopely.fontain.Fontain;
@@ -21,8 +23,28 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fontain.init(this, R.string.default_font_family);
-        Fontain.applyFontToViewHierarchy(getActionBarView(), Fontain.getFontFamily("MedievalSharp"), Weight.NORMAL.value, Width.NORMAL.value, Slope.NORMAL.value);
+        Fontain.applyFontToViewHierarchy(getActionBarView(), Fontain.getFontFamily("MedievalSharp"), Weight.NORMAL, Width.NORMAL, Slope.NORMAL);
         setContentView(R.layout.activity_main);
+
+        ViewGroup container = (ViewGroup) findViewById(R.id.textViewContainer);
+        rotateFonts(container);
+    }
+
+    private void rotateFonts(final ViewGroup container) {
+        final String[] fontFamilies = new String[]{"MedievalSharp", "LS", "PTSans"};
+        final int[] index = new int[]{0};
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Fontain.applyFontFamilyToViewHierarchy(container, Fontain.getFontFamily(fontFamilies[index[0]]));
+                index[0]++;
+                if (index[0] > 2){
+                    index[0] = 0;
+                }
+                container.postDelayed(this, 3000);
+            }
+        };
+        runnable.run();
     }
 
     public View getActionBarView() {
